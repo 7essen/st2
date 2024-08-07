aa9bf70ca4e528c1d9c41433a0194d8048c976b2
7essen <hussona4635@gmail.com>
2024-08-07 17:58:53 +0300

    ss

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,12 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+	upload: {
+		config: {
+			provider: 'st2provider',
+			providerOptions: {
+				token: 'vercel_blob_rw_k6zCgoCCJcYCW0QY_bOWHbKFb0bY7iZvBrrLumDt8lc4ujX',
+				addRandomSuffix: true,
+				cacheControlMaxAge: 31536000, // سنة بالثواني
+			},
+		},
+	},
+});
